d02ff499035a8eb1f215cda5b959b90d8c05c012
BLDR <33762147+negus20@users.noreply.github.com>
2024-02-16 08:44:12 -0700

    fix: test token

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,38 @@
-import { connectToDB } from '@/db';
+import 'dotenv/config';
+import 'reflect-metadata';
+import express from 'express';
+import morgan from 'morgan';
+
+import CheckBetEndsCron from './services/check-bet-ends-cron';
+import { parseEther } from 'ethers';
+
+import logger from '@/utils/logger';
 import ContractEventsListenerGetter from '@/helpers/contract-events-listener-getter';
 import { exampleRoute } from '@/routes/example-route';
 import ContractEventsListenerService from '@/services/contract-events-listener';
-import logger from '@/utils/logger';
-import dotenv from 'dotenv';
-import express from 'express';
-import morgan from 'morgan';
-import TokenService from './services/token';
+import { AppDataSource } from './db';
 
-dotenv.config();
-
-const { CONTRACT_ADDRESS, COINGECKO_API_KEY, COINGECKO_API_URL } = process.env;
+const { CONTRACT_ADDRESS } = process.env;
 
 const port = 3000;
 const app = express();
 
-connectToDB();
+(async () => {
+  try {
+    await AppDataSource.initialize();
+    logger.info('Database connection established successfully.');
+  } catch (error) {
+    logger.error('Error connecting to the database.', error);
+  }
+})();
 
+// TODO: important to make sure that the ABI stored in this repo is up-to-date with the latest version of the smart contract
 const contractEventsListenerGetter = new ContractEventsListenerGetter(
   CONTRACT_ADDRESS!,
   `${__dirname}/contracts/SigmaZero.json`,
 );
 const contractEventsListenerService = new ContractEventsListenerService(
-  contractEventsListenerGetter.getContract(),
+  contractEventsListenerGetter,
 );
 
 contractEventsListenerService.listen();
@@ -36,22 +46,24 @@ const morganMiddleware = morgan(
   },
 );
 
-// TODO: This is just a test as an example. Remove it after using it in the corresponding endpoints.
-const testTokenService = async () => {
-  const tokenService = new TokenService(COINGECKO_API_URL!, COINGECKO_API_KEY!);
+async function testPlaceBet() {
+  try {
+    const betValue = parseEther('000000000000000000000000000000000000000.1');
+    const transaction = await contractEventsListenerGetter
+      .getContract()
+      .placeBet('0xb9ef770b6a5e12e45983c5d80545258aa38f3b78', 60, 0, betValue);
 
-  const coinsList = await tokenService.getCoinsList();
+    await transaction.wait();
 
-  if (coinsList && coinsList.length > 0) {
-    const tokenData = await tokenService.getTokenData('ethereum');
-
-    logger.info(coinsList.find((coin) => coin.id === 'ethereum'));
-    logger.info(tokenData?.market_data.current_price.usd);
-    logger.info(tokenData?.market_data.total_volume.usd);
+    logger.debug('placeBet ok');
+  } catch (error) {
+    logger.error('Error placeBet:', error);
   }
-};
+}
 
-testTokenService();
+testPlaceBet();
+
+new CheckBetEndsCron(contractEventsListenerGetter.getContract()).startCron();
 
 app.use(morganMiddleware);
 
