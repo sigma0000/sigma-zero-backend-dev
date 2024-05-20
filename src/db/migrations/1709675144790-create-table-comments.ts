fb3c533cf580c7acc7591ce35609a4186f9da912
BLDR <33762147+negus20@users.noreply.github.com>
2024-05-19 22:49:54 -0600

    Add comments feature

---
+++ src/db/migrations/1709675144790-create-table-comments.ts
@@ -0,0 +1,60 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateTableComments1709675144790 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'comments',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'bet_id',
+            type: 'uuid',
+          },
+          {
+            name: 'user_id',
+            type: 'int',
+          },
+          {
+            name: 'text',
+            type: 'text',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            columnNames: ['bet_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'bets',
+            onDelete: 'CASCADE',
+          },
+          {
+            columnNames: ['user_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'users',
+            onDelete: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('comments');
+  }
+}
