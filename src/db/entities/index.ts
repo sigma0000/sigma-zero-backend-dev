5a7d54e252b768028ca21440a14a74467c7b4358
BLDR <33762147+negus20@users.noreply.github.com>
2024-10-20 09:46:39 -0600

    Remove legacy entities

--- src/db/entities/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './bet';
-export * from './tokens-snapshot';
-export * from './user';
-export * from './transaction';
-export * from './bet-entry';
-export * from './bet-entry-result';
-export * from './result';
